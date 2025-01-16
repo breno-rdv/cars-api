d3f20d30e88bc35ce55a61038df150a55d4deea2
Breno do Val <brenorvale@gmail.com>
2025-01-16 20:50:57 -0300

    feat: add car domain

---
+++ src/domain/car.ts
@@ -0,0 +1,223 @@
+enum Transmission {
+  MANUAL,
+  AUTOMATIC,
+}
+
+enum FuelType {
+  HIBRID,
+  FLEX,
+  GAS,
+  ETHANOL,
+}
+
+enum CarType {
+  CARS,
+  VANS,
+  MOTORCYCLE,
+}
+
+interface Location {
+  address: string;
+  number: number;
+  city: string;
+  state: string;
+  zipcode: number;
+}
+
+export class Car {
+  private id: string;
+
+  private model: string;
+
+  private make: string;
+
+  private year: string;
+
+  private type: CarType;
+
+  private engineSize: string;
+
+  private fuelType: FuelType;
+
+  private transmission: Transmission;
+
+  private horsepower: string;
+
+  private lastServiceDate: string;
+
+  private mileage: string;
+
+  private licensePlate: string;
+
+  private price: number;
+
+  private createdAt: string;
+
+  private updatedAt: string;
+
+  private location: Location;
+
+  constructor(
+    id: string,
+    model: string,
+    make: string,
+    year: string,
+    type: CarType,
+    engineSize: string,
+    fuelType: FuelType,
+    transmission: Transmission,
+    horsepower: string,
+    lastServiceDate: string,
+    mileage: string,
+    licensePlate: string,
+    price: number,
+    createdAt: string,
+    updatedAt: string,
+    location: Location
+  ) {
+    this.id = id;
+    this.model = model;
+    this.make = make;
+    this.year = year;
+    this.type = type;
+    this.engineSize = engineSize;
+    this.fuelType = fuelType;
+    this.transmission = transmission;
+    this.horsepower = horsepower;
+    this.lastServiceDate = lastServiceDate;
+    this.mileage = mileage;
+    this.licensePlate = licensePlate;
+    this.price = price;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.location = location;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getModel(): string {
+    return this.model;
+  }
+
+  getMake(): string {
+    return this.make;
+  }
+
+  getYear(): string {
+    return this.year;
+  }
+
+  getType(): CarType {
+    return this.type;
+  }
+
+  getEngineSize(): string {
+    return this.engineSize;
+  }
+
+  getFuelType(): FuelType {
+    return this.fuelType;
+  }
+
+  getTransmission(): Transmission {
+    return this.transmission;
+  }
+
+  getHorsepower(): string {
+    return this.horsepower;
+  }
+
+  getLastServiceDate(): string {
+    return this.lastServiceDate;
+  }
+
+  getMileage(): string {
+    return this.mileage;
+  }
+
+  getLicensePlate(): string {
+    return this.licensePlate;
+  }
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  getCreatedAt(): string {
+    return this.createdAt;
+  }
+
+  getUpdatedAt(): string {
+    return this.updatedAt;
+  }
+
+  getLocation(): Location {
+    return this.location;
+  }
+
+  setId(id: string): void {
+    this.id = id;
+  }
+
+  setModel(model: string): void {
+    this.model = model;
+  }
+
+  setMake(make: string): void {
+    this.make = make;
+  }
+
+  setYear(year: string): void {
+    this.year = year;
+  }
+
+  setType(type: CarType): void {
+    this.type = type;
+  }
+
+  setEngineSize(engineSize: string): void {
+    this.engineSize = engineSize;
+  }
+
+  setFuelType(fuelType: FuelType): void {
+    this.fuelType = fuelType;
+  }
+
+  setTransmission(transmission: Transmission): void {
+    this.transmission = transmission;
+  }
+
+  setHorsepower(horsepower: string): void {
+    this.horsepower = horsepower;
+  }
+
+  setLastServiceDate(lastServiceDate: string): void {
+    this.lastServiceDate = lastServiceDate;
+  }
+
+  setMileage(mileage: string): void {
+    this.mileage = mileage;
+  }
+
+  setLicensePlate(licensePlate: string): void {
+    this.licensePlate = licensePlate;
+  }
+
+  setPrice(price: number): void {
+    this.price = price;
+  }
+
+  setCreatedAt(createdAt: string): void {
+    this.createdAt = createdAt;
+  }
+
+  setUpdatedAt(updatedAt: string): void {
+    this.updatedAt = updatedAt;
+  }
+
+  setLocation(location: Location): void {
+    this.location = location;
+  }
+}
