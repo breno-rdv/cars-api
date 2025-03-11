275b446c11d3186697f10617dd2eb91edbac7af6
Breno do Val <brenorvale@gmail.com>
2025-03-11 07:20:01 -0300

    refact: remove data  layer

--- src/data/protocols/add-car-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Car } from "../../domain/car";
-import { AddCarModel } from "../../domain/use-cases/add-car";
-
-export interface AddCarRespository {
-  add(car: AddCarModel): Promise<Car>;
-}
