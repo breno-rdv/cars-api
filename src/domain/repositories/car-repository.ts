31b39fabc10a901315a141526d3ccfc076ebaf6e
Breno do Val <brenorvale@gmail.com>
2025-04-14 16:41:08 -0300

    refact: remove repository layer

--- src/domain/repositories/car-repository.ts
+++
@@ -1,9 +0,0 @@
-import { Car } from "../entities/car";
-// TODO: remove repository and create usecase for each operation
-export interface ICarRepository {
-  create(car: Omit<Car, "id">): Promise<Car>;
-  findById(id: string): Promise<Car | null>;
-  findAll(): Promise<Car[]>;
-  update(id: string, car: Partial<Car>): Promise<Car | null>;
-  delete(id: string): Promise<boolean>;
-}
